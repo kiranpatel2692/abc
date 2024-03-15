e9136fa881001b24f749515797aec178561b3100
kiranpatel2692 <kiranpatel021324@gmail.com>
2024-03-15 19:32:35 -0400

    my second try

---
+++ Client.java
@@ -0,0 +1,29 @@
+package kmp;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cid,phone,cage,cdob;
+		String cname,address,gender;
+		 
+		cid=1;
+		phone=365883707;
+		cdob=02-06-1992;
+		cage=31;
+		cname = "Kiranpatel";
+		gender="male";
+		address="22province st N";
+		
+		
+		System.out.println(cid);
+		System.out.println(phone);
+		System.out.println(cdob);
+		System.out.println(cname);
+		System.out.println(gender);
+		System.out.println(cage);
+		System.out.println(address);
+
+	}
+
+}
